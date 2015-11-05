6d86a0536f79c14857acf0254a969a8ded62d3be
Eric <to.erictran@gmail.com>
2015-11-04 16:37:43 -0800

    First Commit

---
+++ selectionsort.py
@@ -0,0 +1,27 @@
+import random
+arr = random.sample(xrange(1,100),10)
+
+min = 0
+count = 0
+def selectionsort(a):
+	for i in range(0, len(a)):
+		min = i
+		for x in range (i+1, len(a)):
+			if a[x] < a[min]:
+				min = x
+		a[min], a[i] = a[i], a[min]
+
+	print a
+
+selectionsort(arr)
+
+
+
+
+
+		
+
+
+
+
+
